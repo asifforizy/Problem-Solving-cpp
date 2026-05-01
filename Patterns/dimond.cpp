d449df4bae09d80667094465561df25d8f4f3983
MD Saifur Rahaman <asifforizy@gmail.com>
2026-05-01 16:37:55 +0600

    dimond

---
+++ Patterns/dimond.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+void pyramid(int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout << " " ;
+        }
+
+
+        for (int j = 0; j < 2*i+1; j++)
+        {
+            cout << "*" ;
+        }
+
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout << " " ;
+        }
+        cout << endl;
+    }
+}
+
+
+void invertedPyramid(int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " " ;
+        }
+
+        for (int j = 0; j < 2*n-(2*i+1); j++)
+        {
+            cout << "*" ;
+        }
+
+        for (int j = 0; j < i; j++)
+        {
+            cout << " " ;
+        }
+        cout << endl;
+    }
+}
+
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        pyramid(n);
+        invertedPyramid(n);
+    }
+}
