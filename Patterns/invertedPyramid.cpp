db8f9b68f3dee040d21e59fb03d07efe92c90a4d
MD Saifur Rahaman <asifforizy@gmail.com>
2026-05-01 16:34:50 +0600

    inverted pyramid

--- Patterns/invertedPyramid.cpp
+++ Patterns/invertedPyramid.cpp
@@ -6,18 +6,17 @@ void pyramid(int n)
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n-i-1; j++)
+        for (int j = 0; j < i; j++)
         {
             cout << " " ;
         }
 
-
-        for (int j = 0; j < 2*i+1; j++)
+        for (int j = 0; j < 2*n-(2*i+1); j++)
         {
             cout << "*" ;
         }
 
-        for (int j = 0; j < n-i-1; j++)
+        for (int j = 0; j < i; j++)
         {
             cout << " " ;
         }
