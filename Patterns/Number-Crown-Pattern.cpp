0d8b57d8198a047ddf925aa571e754a8e8973463
MD Saifur Rahaman <asifforizy@gmail.com>
2026-05-02 20:20:56 +0600

    number crown pattern

---
+++ Patterns/Number-Crown-Pattern.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void pyramid(int n)
+{
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+
+        for (int j = 1; j <= 2 * (n - i); j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        pyramid(n);
+    }
+}
