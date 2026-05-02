7610d718f3581dd4afbe0b7b372bdeafdcb22100
MD Saifur Rahaman <asifforizy@gmail.com>
2026-05-02 20:31:58 +0600

    increasing number

---
+++ Patterns/Increasing-Letter-Triangle.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void  increasingLatter(int n)
+{
+
+    int number = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <=  'A'+ i; ch++)
+        {
+            cout <<  ch;
+            
+        }
+
+        cout << endl;
+    }
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        increasingLatter(n);
+    }
+}
